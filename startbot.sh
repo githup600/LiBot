c70d634627c4112e09546d63e057cb533070568b
Nimsilu <101490767+Nimsilu@users.noreply.github.com>
2022-03-25 00:13:14 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -7,4 +7,4 @@ chmod +x engines/fairy-sf
 
 python heroku_challenges.py         # This start point will accept challenges according to the config.yml
 
-# python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
+ python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
