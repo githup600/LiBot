47b41d6d40990003deec7e8d0790be4dc05dc2b6
Nimsilu <101490767+Nimsilu@users.noreply.github.com>
2022-03-27 17:21:26 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,15 +1,8 @@
- build:
+build:
   docker:
     worker: Dockerfile
-
-
-#!/bin/bash
-chmod +x engines/stockfish
-chmod +x engines/multivariant_stockfish
-chmod +x engines/fairy-sf
-
-### ONLY UNCOMMENT ONE START POINT ###
-
-python heroku_challenges.py         # This start point will accept challenges according to the config.yml
-
-#python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
+run:
+  worker:
+    command:
+      - python3 heroku_challenges.py
+    image: worker
